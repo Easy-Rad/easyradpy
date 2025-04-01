2eeacee93f8d963bbd14b3546810d969dabacb4a
Tubo Shi <tubo@tubo.nz>
2025-04-01 20:08:04 +1300

    Refactor main script to streamline AHK executable handling; update .gitignore to include dist_resources directory

--- src/main.py
+++ src/main.py
@@ -2,26 +2,15 @@ import os
 import sys
 from ahk import AHK
 
-def get_bundle_dir():
-    """Get the directory where the bundled files are stored."""
-    if getattr(sys, 'frozen', False):
-        # Running as compiled executable
-        return os.path.dirname(sys.executable)
-    else:
-        # Running as script
-        return os.path.dirname(os.path.abspath(__file__))
 
 def show_message():
     # Set the path to the bundled AHK executable if running as a compiled executable
     if getattr(sys, 'frozen', False):
-        bundle_dir = get_bundle_dir()
-        ahk_exe_path = os.path.join(bundle_dir, "AutoHotkey.exe")
-        if os.path.exists(ahk_exe_path):
-            os.environ["AHK_PATH"] = ahk_exe_path
+        ahk = AHK(executable_path = os.path.join(getattr(sys, "_MEIPASS", os.path.dirname(os.path.realpath(sys.executable))), "AutoHotkey.exe"))
     
-    # Initialize AHK
-    ahk = AHK(version="v2")
-    
+    else:
+        ahk = AHK(version="v2")
+        
     # Show a message box directly using AHK
     ahk.msg_box('This is a test')
 
